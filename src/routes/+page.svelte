f068b0121d65a27e51a7a078ed0d9e2b4c8ba00f
sharath <sharath.dt@gmail.com>
2023-05-27 11:06:20 +0530

    add unocss webfonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <div grid place-items-center mt-10>
   <div class="text-center">
-    <div w-auto text="5xl gray-400 hover:green-600" i-ic-baseline-grass />
-    <h1>Welcome to Mykit</h1>
-    <p>A starter kit for building Sveltekit websites and apps.</p>
+    <div w-auto text="5xl green-600 hover:green-700" i-ic-baseline-grass />
+    <h1 class="font-lato text-4xl font-bold mb-3">Welcome to Mykit</h1>
+    <p class="mb-2">A starter kit for building Sveltekit websites and apps.</p>
   </div>
-  <ul class="list-disc">
+  <ul class="list-disc font-mono">
     <li>Sveltekit</li>
     <li>UnoCSS</li>
+    <ul class="ml-8 text-sm list-disc">
+      <li>Tailwind support</li>
+      <li>CSS icons</li>
+      <li>Webfonts</li>
+    </ul>
     <li>Mdsvex</li>
   </ul>
 </div>
--- uno.config.js
+++ uno.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from "unocss";
 import presetUno from "@unocss/preset-uno";
 import presetIcons from "@unocss/preset-icons";
-import presetAttributify from '@unocss/preset-attributify'
+import presetAttributify from "@unocss/preset-attributify";
+import presetWebFonts from "@unocss/preset-web-fonts";
+import axios from "axios";
+import ProxyAgent from "proxy-agent";
 
 export default defineConfig({
-  presets: [presetAttributify({}), presetIcons({}), presetUno()],
+  presets: [
+    presetUno(),
+    presetAttributify({}),
+    presetIcons({}),
+    presetWebFonts({
+      provider: "google", // default provider
+      fonts: {
+        // these will extend the default theme
+        sans: "Roboto",
+        mono: ["Fira Code", "Fira Mono:400,700"],
+        // custom ones
+        lobster: "Lobster",
+        lato: [
+          {
+            name: "Lato",
+            weights: ["400", "700"],
+            italic: true,
+          },
+          {
+            name: "sans-serif",
+            provider: "none",
+          },
+        ],
+      },
+    }),
+  ],
 });
