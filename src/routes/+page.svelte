60d03ef848ad60c9023b8067a280b12b061d57ea
sharath <sharath.dt@gmail.com>
2024-03-30 21:28:00 +0530

    update to svelte 5 and sveltekit 2 🤭

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,27 @@
-<div grid place-items-center mt-10>
-  <div class="text-center">
-    <div w-auto text="5xl green-600 hover:green-700" i-ic-baseline-grass />
-    <h1 class="font-lato text-4xl font-bold mb-3">Welcome to Mykit</h1>
+<div class="grid place-items-center h-screen">
+  <div class="grid place-items-center">
+    <div class="flex text-center">
+      <div class="i-tabler-tools-kitchen text-5xl text-orange-600 mr-1" />
+      <h1 class="font-lato text-5xl font-bold mb-3">mykit</h1>
+    </div>
     <p class="mb-2">A starter kit for building Sveltekit websites and apps.</p>
+    <ul class="list-disc font-mono">
+      <li>Sveltekit</li>
+      <li>UnoCSS</li>
+      <ul class="ml-8 text-sm list-disc">
+        <li>Tailwind support</li>
+        <li>CSS icons</li>
+      </ul>
+      <li>Mdsvex</li>
+      <ul class="ml-8 text-sm list-disc">
+        <li>Custom layouts</li>
+        <li>
+          Sample <a class="underline" href="/about">page</a> and
+          <a class="underline" href="/blog/a-sample-post">post</a>
+        </li>
+      </ul>
+    </ul>
   </div>
-  <ul class="list-disc font-mono">
-    <li>Sveltekit</li>
-    <li>UnoCSS</li>
-    <ul class="ml-8 text-sm list-disc">
-      <li>Tailwind support</li>
-      <li>CSS icons</li>
-    </ul>
-    <li>Mdsvex</li>
-    <ul class="ml-8 text-sm list-disc">
-      <li>Custom layouts</li>
-      <li>
-        Sample <a class="underline" href="/about">page</a> and
-        <a class="underline" href="/blog/a-sample-post">post</a>
-      </li>
-    </ul>
-  </ul>
 </div>
 
 <svelte:head>
--- uno.config.js
+++ uno.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "unocss";
 import presetUno from "@unocss/preset-uno";
 import presetIcons from "@unocss/preset-icons";
-import presetAttributify from "@unocss/preset-attributify";
 
 export default defineConfig({
   presets: [
     presetUno(),
-    presetAttributify({}),
     presetIcons({}),
   ],
 });
