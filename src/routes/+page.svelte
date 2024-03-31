a708c36ae84f503289b1259996c1e721a5207d2e
sharath <sharath.dt@gmail.com>
2024-03-31 09:06:26 +0530

    update title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   <div class="grid place-items-center">
     <div class="flex text-center">
       <div class="i-tabler-tools-kitchen text-5xl text-orange-600 mr-1" />
-      <h1 class="font-lato text-5xl font-bold mb-3">mykit</h1>
+      <h1 class="font-lato text-5xl font-bold mb-3">SumoKit</h1>
     </div>
     <p class="mb-2">A starter kit for building Sveltekit websites and apps.</p>
     <ul class="list-disc font-mono">
